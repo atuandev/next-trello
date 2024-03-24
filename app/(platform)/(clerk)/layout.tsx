8924880b8c5d4551a681184c007bd1ac4d24d579
Boi <anhtuan03.dev@gmail.com>
2024-03-24 18:31:20 +0700

    feat: set up Clerk

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,11 @@
+export default function ClerkLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex h-full items-center justify-center">
+      {children}
+    </div>
+  )
+}
